18bcc4854a85ce23d5c0609d6f932c5bc5129cdf
LeahZeevi <leah161821@gmail.com>
2025-04-21 21:11:02 +0300

    Revert "login and register"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,12 @@
 import { RouterProvider } from 'react-router'
 import './App.css'
 import router from './routes/AppRoute'
-// import A1 from './pages/A1'
 
 
 function App() {
   return (
     <>
    <RouterProvider router={router}/>
-   {/* <A1 city="Jerusalem" /> */}
-
     </>
   )
 }
