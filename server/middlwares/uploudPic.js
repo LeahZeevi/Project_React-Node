4f2c99b85b60f6e721cf4bc149a7a89092011a3a
LeahZeevi <leah161821@gmail.com>
2025-04-26 22:11:47 +0300

    Update uploudPic.js

--- server/middlwares/uploudPic.js
+++ server/middlwares/uploudPic.js
@@ -1,23 +1,23 @@
-const multer = require('multer');
-const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null,"../public/uploadsPic")
-    },
-    filename: (req, file, callback) => {
-        callback(null, `${Date.now()}-${file.originalname}`)
-    }
-})
-const fileFilter = (req, file, callback) => {
-    if (file.mimtype === 'image/jpeg' || file.mimtype === 'image/png'
-        || file.mimtype === 'image/jpg') {
-        callback(null, true);
-    }
-    callback(null, false)
+// const multer = require('multer');
+// const storage = multer.diskStorage({
+//     destination: (req, file, callback) => {
+//         callback(null,"../public/uploadsPic")
+//     },
+//     filename: (req, file, callback) => {
+//         callback(null, `${Date.now()}-${file.originalname}`)
+//     }
+// })
+// const fileFilter = (req, file, callback) => {
+//     if (file.mimtype === 'image/jpeg' || file.mimtype === 'image/png'
+//         || file.mimtype === 'image/jpg') {
+//         callback(null, true);
+//     }
+//     callback(null, false)
 
-}
+// }
 
-const uploudPic = multer({
-    storage,
-    fileFilter,
-})
-module.exports = uploudPic;
+// const uploudPic = multer({
+//     storage,
+//     fileFilter,
+// })
+// module.exports = uploudPic;
