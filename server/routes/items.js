bf0bd7f9b67b0d7a257e922df46e130a28bd53d8
LeahZeevi <leah161821@gmail.com>
2025-04-26 22:09:35 +0300

    Update items.js

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,20 +1,20 @@
 const express=require("express")
 console.log("enter rout");
 
-// const router=express.Router()
-//  const Item=require("../models/items")
-// const verifyJWT=require("../middlwares/verifyJWT")
-// const {addItem,updateItem,getItemById,deletItem, getItemsByCategoryId} =require("../controllers/items");
-// const uploudPic = require("../middlwares/uploudPic");
-// console.log("enter rout");
+const router=express.Router()
+ const Item=require("../models/items")
+const verifyJWT=require("../middlwares/verifyJWT")
+const {addItem,updateItem,getItemById,deletItem, getItemsByCategoryId} =require("../controllers/items");
+const uploudPic = require("../middlwares/uploudPic");
+console.log("enter rout");
 
 
 
-// router.post('/',uploudPic.single('url'),addItem);
-// // router.get('/:_id',getItemById);
-// router.get('/category/:categoryId',getItemsByCategoryId);
-// router.patch('/:_id',updateItem);
-// router.delete('/:_id',deletItem);
+router.post('/',uploudPic.single('url'),addItem);
+router.get('/:_id',getItemById);
+router.get('/category/:categoryId',getItemsByCategoryId);
+router.patch('/:_id',updateItem);
+router.delete('/:_id',deletItem);
 // // router.post('/',addItem);
 // // router.post('/',verifyJWT, uploudPic.single('url'), addItem);  // שימוש במולטר להעלאת תמונה
 // // router.get('/:id',verifyJWT, getItemById);
@@ -22,4 +22,4 @@ console.log("enter rout");
 // // router.patch('/:id',verifyJWT, updateItem);
 // // router.delete('/:id',verifyJWT, deletItem);
 
-module.exports=router;
+
