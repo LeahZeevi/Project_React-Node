d1f2bf9dad46d44ae21091fc121026f64f906e9f
LeahZeevi <leah161821@gmail.com>
2025-04-26 22:05:32 +0300

    Update items.js

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -3,6 +3,30 @@ const Item = require("../models/items")
 // const { saveImage } = require('../middlware/uploudPic');
 const User = require('../models/users');
 
+exports.addItem = async (req, res) => {
+    console.log(req.file);
+       const {ItemName,url,categoryName,season,categoryId,inUse,countWear,style}=req.body
+     const { path :image}=req.file.path
+
+    if(!ItemName||!categoryName||!season){
+        return res.status(400).json({message:"item with this name is exist"})
+    }
+        //lean(): המרה לקריאה בלבד מזרז את תהליך השאילתה
+    const duplicate=await User.findOne({ItemName:ItemName}).lean()
+    if(duplicate){
+        console.log(duplicate);
+       return res.status(409).json({message:"Dupliacated ItemName"})}
+      
+         const itemObject={ItemName,url:req.file.path,categoryName,season,categoryId,inUse,countWear,style}
+        const item=await Item.create(itemObject)
+
+       if(item)
+               return res,status(201).json({message:`New item ${item.ItemName } created`})
+            else
+            return res,status(400).json({message:`Invalid Item received`})
+        
+            
+        }
 
 
 exports.getItemById = async (req, res) => {
