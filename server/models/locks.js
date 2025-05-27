ec155b985b28b1bad663e81f7ba5c1d45fc6ad0c
Tami Medan <tamimedan2880@gmail.com>
2025-05-27 19:08:12 +0300

    nothing to commit

--- server/models/locks.js
+++ server/models/locks.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const locksSchema = new mongoose.Schema({
-    user_id: { type: String, required: true},
-    itemsInlock: [{ type: mongoose.Types.ObjectId, require: true, ref: 'items' }],
+    user_id: { type: String, required: true },
+    itemsInlock: [{ type: Item, require: true, ref: 'items' }],
     dateCreation: {
         type: Date,
         default: Date.now
