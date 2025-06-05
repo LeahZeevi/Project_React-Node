af6e04f16e6f223fb64ac7fcb5590d87102add48
leah zeevi <leah161821@gmail.com>
2025-06-05 11:37:36 +0300

    finish fixed code

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -8,7 +8,8 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://localhost:5175',
   'http://localhost:5176',
-  'http://localhost:5177'
+  'http://localhost:5177',
+  'http://localhost:5178'
 
 ];
 const corsOptions = {
